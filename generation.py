f5d059acb138b62764c0d3444d1b7c7244031c10
JunjieChiang <chunchieh@DESKTOP-RHIRPID>
2024-04-22 21:52:40 +0800

    update generation.py

--- generation.py
+++ generation.py
@@ -3,6 +3,7 @@ from http import HTTPStatus
 import logging
 import dashscope
 import json
+import argparse
 import info_retrieval
 
 def integrate_docs_to_context(relevant_docs):
@@ -14,10 +15,40 @@ def integrate_docs_to_context(relevant_docs):
 def call_with_messages(query, context):
 
     prompt = f"""
-    你将接收到一些使用json字典格式存储的基础资料对象用于解答用户的询问。该对象是面向BIM模型中各个基础构建的JSON数据对象，这些对象具有以下键及其结构：
-    name：构件名称，global_id：构件id，properties：一个包含额外属性的对象集。其中有一些构件属性会直接显示在构件名称中，你需要根据用户提到的构件名称到其相应的properties中进行相关信息的查询。
-    '''{context}'''。
-    此外，你接收到的用户问题是：'''{query}'''。
+    你将接收到一些面向BIM模型中各基础构件的JSON数据对象，这些对象具有以下键及其结构：name：构件名称，global_id：构件id，properties：该构件的属性集，该属性集及其相应解释如下：
+        "IsExternal": 该构件是否在外部？,
+        "LoadBearing": 该构件是否是承重结构构件？,
+        "ExtendToStructure": 该构件是否延伸到了结构?,
+        "Slope": 该构件的倾斜程度,
+        "Reference": 该构件的型号,
+        "Span": 该构件的间隔范围,
+        "类别": 该构件的类别,
+        "族": 该构件的族,
+        "族与类型": 该构件的族与类型,
+        "类型": 该构件类型,
+        "类型 ID": 该构件类型 ID,
+        "连接状态": 该构件的连接状态,
+        "体积": 该构件的体积,
+        "底部高程": 该构件的底部高程,
+        "长度": 该构件的长度,
+        "顶部高程": 该构件的顶部高程,
+        "结构材质": 该构件的结构材质,
+        "部件名称": 该构件的部件名称,
+        "参照标高": 该构件的参照标高,
+        "参照标高高程": 该构件的参照标高高程,
+        "横截面旋转": 该构件的横截面旋转,
+        "终点标高偏移": 该构件的终点标高偏移,
+        "起点标高偏移": 该构件的起点标高偏移,
+        "结构用途": 该构件的结构用途,
+        "剪切长度": 该构件的剪切长度,
+        "创建的阶段": 该构件的创建阶段,
+        "OverallHeight": 该构件的高度,
+        "OverallWeight": 该构件的宽度
+    检索到的JSON对象中包含了name, global_id和properties，properties中有键和值，键指的是属性名称，值则是具体的属性值。你需要根据上面对属性集“键”的解释以及以下检索到的JSON对象中相应“键“的“值”来回答用户的查询需求。
+    检索到的相关JSON对象如下：
+    '''{context}'''
+    你接收到的用户问题是：
+    '''{query}'''
     """
 
     messages = [{'role': 'system', 'content': 'You are a professional assistant for construction managers.'},
@@ -38,15 +69,28 @@ def call_with_messages(query, context):
             response.code, response.message
         ))
 
-query = '单扇窗高度是多高？'
-data_path = 'examples/BIM.json'
-model_path = 'retriever/multilingual-e5-large'
-index_path = 'result/faiss_index.index'
-json_name_path = 'result/json_obj_name.json'
-relevant_docs = info_retrieval.main(query, data_path, model_path, index_path, json_name_path)
-context = integrate_docs_to_context(relevant_docs)
-call_with_messages(query, context)
+
+parser = argparse.ArgumentParser(description="Retrieve information and generate answers for BIM model queries.")
+parser.add_argument('--query', type=str, default='嵌入到内部的基本墙有多厚？',
+                    help="The user query to process.")
+parser.add_argument('--embedding_model', type=str, default='retriever\multilingual-e5-large',
+                    help="Retrieve relevant information, the model can be replaced by model name (directory: retriever)")
+parser.add_argument('--data_path', type=str, default='examples/BIM.json',
+                    help='original data source need to search')
+parser.add_argument('--index_path', type=str, default='result/faiss_index.index',
+                    help='speed up the searching process')
+parser.add_argument('--vectorised_KB', type=str, default='result/embeddings.json',
+                    help='embedded external knowledge')
+parser.add_argument('--json_name_path', type=str, default='result/json_obj_name.json',
+                    help='searching information according retrieved name')
+parser.add_argument('--top_k', type=int, default=5,
+                    help='the number of returned information chunks')
+
+args = parser.parse_args()
+
 
 if __name__ == '__main__':
 
-    print()
+    relevant_docs = info_retrieval.main(args.query, args.data_path, args.index_path, args.vectorised_KB, args.json_name_path, args.top_k)
+    context = integrate_docs_to_context(relevant_docs)
+    call_with_messages(args.query, context)
--- info_retrieval.py
+++ info_retrieval.py
@@ -2,6 +2,7 @@ import json
 import logging
 import numpy as np
 import faiss
+import argparse
 from FlagEmbedding import FlagModel
 import vectorized_KB
 
@@ -9,6 +10,7 @@ import vectorized_KB
 def setup_logging():
 
     logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
 
 
 def load_faiss_index(index_file):
@@ -40,12 +42,12 @@ def encode_query(model, query):
     return np.array([query_vector]).astype('float32')
 
 
-def search_index(index, names, query, query_vector, data, k=1):
+def search_index(index, names, query, query_vector, data, top_k):
 
     if index is not None:
-        D, I = index.search(query_vector, k)
+        D, I = index.search(query_vector, top_k)
         logging.info(f"User query: {query}")
-        logging.info(f"Top {k} most similar items indices: {I}")
+        logging.info(f"Top {top_k} most similar items indices: {I}")
         logging.info(f"Corresponding distances: {D}")
 
         matched_names = [names[idx] for idx in I[0]]        # 获取匹配的"name"
@@ -66,6 +68,7 @@ def search_index(index, names, query, query_vector, data, k=1):
 
         return [], []
 
+'''
 def main(query, data_path, model_path, index_path, json_name_path):
 
     setup_logging()
@@ -81,14 +84,59 @@ def main(query, data_path, model_path, index_path, json_name_path):
     # print(relevant_docs)
 
     return relevant_docs
+'''
+
+def load_resources(data_file, index_file, embeddings_file, names_file):
+
+    with open(data_file, 'r', encoding='utf-8') as file:
+        data = json.load(file)
+    logging.info(f"JSON {data_file} Uploaded")
+
+    with open(embeddings_file, 'r', encoding='utf-8') as file:
+        embeddings = json.load(file)  # 加载已保存的embeddings
+    logging.info(f"Embeddings uploaded.")
+
+    index = faiss.read_index(index_file)    # 加载已保存的FAISS索引
+    logging.info(f"FAISS index loaded with {index.ntotal} vectors.")
+
+    with open(names_file, 'r', encoding='utf-8') as file:
+        names = json.load(file)            # 加载已保存的names
+    logging.info(f"JSON name loaded")
+
+    return data, index, embeddings, names
+
+
+def main(query, data_file, index_file, embeddings_file, names_file, top_k):
+    setup_logging()
+
+    # 创建模型实例（确保模型实例可以重用，或者仅在需要时创建）
+    model = vectorized_KB.load_embedding_model('retriever/multilingual-e5-large')
+
+    # 加载预先计算的资源
+    data, index, embeddings, names = load_resources(data_file, index_file, embeddings_file, names_file)
+
+    logging.info("ALL precomputed resources loaded successfully.")
+
+    # 向量化查询
+    query_vector = encode_query(model, query)
+
+    # 执行搜索
+    matched_names, matched_items = search_index(index, names, query, query_vector, data, top_k)
+
+    # 处理匹配到的文档
+    relevant_docs = [item['properties'] for item in matched_items]
+
+    return relevant_docs
 
 
 if __name__ == "__main__":
-    query = '请问砖墙的厚度有多厚？'
-    data_path = 'examples/BIM.json'
-    model_path = 'retriever/multilingual-e5-large'
-    index_path = 'result/faiss_index.index'
-    json_name_path = 'result/json_obj_name.json'
+    parser = argparse.ArgumentParser(description="Retrieve information from precomputed BIM model data.")
+    parser.add_argument("query", help="Query string to process")
+    args = parser.parse_args()
 
-    main(query, data_path, model_path, index_path, json_name_path)
+    index_path = 'result/faiss_index.index'
+    embeddings_path = 'result/embeddings.json'
+    names_path = 'result/names.json'
+
+    # relevant_docs = main(args.query, index_path, embeddings_path, names_path)
 
